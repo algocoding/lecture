b4ca46dcca90061190483f1393db30a6e76999d0
algocoding <algocoding.net@gmail.com>
2017-05-27 20:33:55 +0900

    Update conf.py

--- conf.py
+++ conf.py
@@ -41,8 +41,7 @@ extensions = [
     'sphinx.ext.mathjax',    # rst2pdf와 같이 사용 안됨.
     'sphinx.ext.ifconfig',    
     'sphinx.ext.githubpages',	
-	'sphinx.ext.doctest',    
-	#'rst2pdf.pdfbuilder',
+    'sphinx.ext.doctest',    
     'sphinxcontrib.disqus',			# disqus 추가 -----------------------		
 ]
 
@@ -229,7 +228,7 @@ html_show_sourcelink = False
 #html_search_scorer = 'scorer.js'
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'algocodingdoc'
+#htmlhelp_basename = 'algocodingdoc'
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -241,7 +240,7 @@ latex_elements = {
 'pointsize': '11pt',
 
 # Additional stuff for the LaTeX preamble.
-'preamble': r'\usepackage{kotex}',
+#'preamble': r'\usepackage{kotex}',
 
 # Latex figure (float) alignment
 #'figure_align': 'htbp',
@@ -312,149 +311,6 @@ texinfo_documents = [
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
 
-# -------------------------------------------------------------------------
-# -- Options for PDF output ----------------------------------------------
-# Grouping the document tree into PDF files. List of tuples
-# (source start file, target name, title, author, options).
-#
-# If there is more than one author, separate them with \\.
-# For example: r'Guido van Rossum\\Fred L. Drake, Jr., editor'
-#
-# The options element is a dictionary that lets you override
-# this config per-document.
-# For example,
-# ('index', u'MyProject', u'My Project', u'Author Name',
-# dict(pdf_compressed = True))
-# would mean that specific document would be compressed
-# regardless of the global pdf_compressed setting.
-pdf_documents = [
- ('index', u'AlgoCoding', u'AlgoCoding rst2pdf doc', u'JongYun Jung'),
-]
-# A comma-separated list of custom stylesheets. Example:
-pdf_stylesheets = ['sphinx','kerning','a4']
-# A list of folders to search for stylesheets. Example:
-pdf_style_path = ['.', '_styles']
-# Create a compressed PDF
-# Use True/False or 1/0
-# Example: compressed=True
-#pdf_compressed = False
-# A colon-separated list of folders to search for fonts. Example:
-# pdf_font_path = ['/usr/share/fonts', '/usr/share/texmf-dist/fonts/']
-# Language to be used for hyphenation support
-pdf_language = "ko_KR"
-# Mode for literal blocks wider than the frame. Can be
-# overflow, shrink or truncate
-#pdf_fit_mode = "shrink"
-# Section level that forces a break page.
-# For example: 1 means top-level sections start in a new page
-# 0 means disabled
-#pdf_break_level = 0
-# When a section starts in a new page, force it to be 'even', 'odd',
-# or just use 'any'
-#pdf_breakside = 'any'
-# Insert footnotes where they are defined instead of
-# at the end.
-#pdf_inline_footnotes = True
-# verbosity level. 0 1 or 2
-#pdf_verbosity = 0
-# If false, no index is generated.
-#pdf_use_index = True
-# If false, no modindex is generated.
-#pdf_use_modindex = True
-# If false, no coverpage is generated.
-#pdf_use_coverpage = True
-# Name of the cover page template to use
-#pdf_cover_template = 'sphinxcover.tmpl'
-# Documents to append as an appendix to all manuals.
-#pdf_appendices = []
-# Enable experimental feature to split table cells. Use it
-# if you get "DelayedTable too big" errors
-#pdf_splittables = False
-# Set the default DPI for images
-#pdf_default_dpi = 72
-# Enable rst2pdf extension modules (default is only vectorpdf)
-# you need vectorpdf if you want to use sphinx's graphviz support
-#pdf_extensions = ['vectorpdf']
-# Page template name for "regular" pages
-#pdf_page_template = 'cutePage'
-# Show Table Of Contents at the beginning?
-#pdf_use_toc = True
-# How many levels deep should the table of contents be?
-pdf_toc_depth = 9999
-# Add section number to section references
-pdf_use_numbered_links = False
-# Background images fitting mode
-pdf_fit_background_mode = 'scale'
-
-
-# -- Options for Epub output ----------------------------------------------
-
-# Bibliographic Dublin Core info.
-epub_title = project
-epub_author = author
-epub_publisher = author
-epub_copyright = copyright
-
-# The basename for the epub file. It defaults to the project name.
-#epub_basename = project
-
-# The HTML theme for the epub output. Since the default themes are not
-# optimized for small screen space, using the same theme for HTML and epub
-# output is usually not wise. This defaults to 'epub', a theme designed to save
-# visual space.
-#epub_theme = 'epub'
-
-# The language of the text. It defaults to the language option
-# or 'en' if the language is not set.
-#epub_language = ''
-
-# The scheme of the identifier. Typical schemes are ISBN or URL.
-#epub_scheme = ''
-
-# The unique identifier of the text. This can be a ISBN number
-# or the project homepage.
-#epub_identifier = ''
-
-# A unique identification for the text.
-#epub_uid = ''
-
-# A tuple containing the cover image and cover page html template filenames.
-#epub_cover = ()
-
-# A sequence of (type, uri, title) tuples for the guide element of content.opf.
-#epub_guide = ()
-
-# HTML files that should be inserted before the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_pre_files = []
-
-# HTML files that should be inserted after the pages created by sphinx.
-# The format is a list of tuples containing the path and title.
-#epub_post_files = []
-
-# A list of files that should not be packed into the epub file.
-epub_exclude_files = ['search.html']
-
-# The depth of the table of contents in toc.ncx.
-#epub_tocdepth = 3
-
-# Allow duplicate toc entries.
-#epub_tocdup = True
-
-# Choose between 'default' and 'includehidden'.
-#epub_tocscope = 'default'
-
-# Fix unsupported image types using the Pillow.
-#epub_fix_images = False
-
-# Scale large images.
-#epub_max_image_width = 0
-
-# How to display URL addresses: 'footnote', 'no', or 'inline'.
-#epub_show_urls = 'inline'
-
-# If false, no index is generated.
-#epub_use_index = True
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
